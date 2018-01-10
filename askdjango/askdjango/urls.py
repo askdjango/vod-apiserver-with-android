fcce5e7411a10c5823f428a3461a64e94ac5b4f1
Chinseok Lee <me@nomade.kr>
2018-01-10 11:29:53 +0900

    MEDIA/STATIC 설정

--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -22,4 +24,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_auth_token),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
