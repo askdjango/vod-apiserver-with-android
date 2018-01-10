4a23b2826a60b820ead857065d0c636fb1775169
Chinseok Lee <me@nomade.kr>
2018-01-10 11:32:11 +0900

    blog.Post 모델 추가 및 마이그레이션

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    message = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
+
